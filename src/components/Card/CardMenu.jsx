3723345977da1ff6333654ec981a1df285fcbe75
RamDiv <142648607+Bintooot@users.noreply.github.com>
2024-12-26 21:27:02 +0800

    Modified

--- src/components/Card/CardMenu.jsx
+++ src/components/Card/CardMenu.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CardMenu = ({ title, number, icon }) => {
   return (
     <div className="flex items-center justify-between bg-green-500 h-32 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200">
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center gap-2 px-2">
         <div className="text-3xl text-white">{icon}</div>
         <div>
           <h3 className="text-lg font-semibold truncate text-white text-wrap">
