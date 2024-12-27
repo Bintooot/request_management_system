86eb07e6f08a36300af9a3c7702e65e2c1c488bd
RamDiv <142648607+Bintooot@users.noreply.github.com>
2024-12-27 09:06:52 +0800

    crating new pages for user

--- src/view/user/Inqure.jsx
+++ src/view/user/Inqure.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "../../components/Header/Header";
+import Sidebar from "../../components/Navigation/FixedSidebar";
 
 const Inqure = () => {
-  return <div></div>;
+  const [Open, setOpen] = useState(false);
+
+  const togglehandler = () => {
+    setOpen(!Open);
+  };
+  return (
+    <div>
+      <Header togglehandler={togglehandler} />
+      {Open && (
+        <div className="fixed inset-0 top-20 bg-green-900 bg-opacity-100 z-50">
+          <Sidebar />
+        </div>
+      )}
+      <div className="flex">
+        <div className="bg-green-900 max-w-60 w-full h-screen md:block hidden">
+          <Sidebar />
+        </div>
+        <div>
+          <main></main>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Inqure;
